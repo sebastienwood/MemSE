69f2a4f21c9dbe86a55926abd24f6d27f704a160
sebastienwood <sebastien.henwood@polymtl.ca>
2022-08-08 18:17:48 -0400

    profile file

---
+++ MemSE/nn/op/numba_profiling.py
@@ -0,0 +1,19 @@
+import torch
+from numba import cuda
+from .conv2duf_op import conv2duf_op
+
+device = torch.device('cuda:0')
+wh = 32
+who = wh - 2
+ch = 3
+bs = 32
+
+input = torch.rand(bs, ch, who, who, ch, who, who).to(device)
+gamma = torch.rand(bs, ch, wh, wh, ch, wh, wh).to(device)
+mu = torch.rand(bs, ch, wh, wh).to(device)
+c = torch.tensor(1.).to(device)
+if c.dim() == 0:
+    c = c.repeat(input.shape[1])
+weight_shape = [ch, ch, 3, 3]
+with cuda.profiling():
+    conv2duf_op(input, gamma, mu, c, weight_shape)
