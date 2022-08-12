6b5e517e262425a3533bbe9e39c3b7998125cd35
sebastienwood <sebastien.henwood@polymtl.ca>
2022-08-12 09:00:18 -0400

    new unpassing test

--- tests/test_network_manipulations.py
+++ tests/test_network_manipulations.py
@@ -4,18 +4,21 @@ import pytest
 
 from MemSE.network_manipulations import build_sequential_linear, conv_to_fc, conv_to_unfolded, record_shapes, fuse_conv_bn
 from MemSE.nn import Conv2DUF
-from MemSE.models import smallest_vgg, resnet18
+from MemSE.models import smallest_vgg, resnet18, smallest_vgg_ReLU
+from MemSE import MemristorQuant, MemSE
 
 torch.manual_seed(0)
-inp = torch.rand(1,3,32,32)
+inp = torch.rand(2,3,32,32)
 conv = nn.Conv2d(3,3,2)
 smallest_vgg_ = smallest_vgg()
+smallest_vgg_ReLU_ = smallest_vgg_ReLU()
 resnet18_ = fuse_conv_bn(resnet18().eval(), 'resnet18')
 
 MODELS = [
     conv,
     smallest_vgg_,
-    resnet18_
+    resnet18_,
+    smallest_vgg_ReLU_
 ]
 
 
@@ -26,6 +29,9 @@ def test_memristor_unfolded(net):
     y_hat = conv2duf(inp)
     assert y.shape == y_hat.shape
     assert torch.allclose(y, y_hat, rtol=1e-3)
+    quanter = MemristorQuant(net, std_noise=0.01, Gmax = 3.268, N=100000)
+    memse = MemSE(net, quanter, input_bias=False)
+    memse.no_power_forward(inp)
 
 
 @pytest.mark.parametrize("net", MODELS)
@@ -35,3 +41,6 @@ def test_memristor_large(net):
     y_hat = conv2duf(inp)
     assert y.shape == y_hat.shape
     assert torch.allclose(y, y_hat, rtol=1e-3)
+    quanter = MemristorQuant(net, std_noise=0.01, Gmax = 3.268, N=100000)
+    memse = MemSE(net, quanter, input_bias=False)
+    memse.no_power_forward(inp)
