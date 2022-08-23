2fa6c568722b9d7c90964279a999e609b8610ec5
sebastienwood <sebastien.henwood@polymtl.ca>
2022-08-22 23:42:14 -0400

    fix relu derivative

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from MemSE.nn.utils import zero_diag
+from MemSE.nn import ReLU
 
 BS = 8
 C = 3
@@ -15,3 +16,17 @@ def test_zero_diag():
     batched_cov = torch.ones(BS, C, WH, WH, C, WH, WH)
     res = zero_diag(batched_cov)
     assert res.sum() == batched_cov.sum() - C * WH * WH * BS
+
+
+def test_relu_derivative():
+    mu = torch.arange(-10, -1)
+    grad = ReLU.derivatives(None, None, mu)[1]
+    assert torch.all(grad == 0)
+
+    mu = torch.arange(1, 10)
+    grad = ReLU.derivatives(None, None, mu)[1]
+    assert torch.all(grad == 1)
+
+    mu = torch.Tensor([0])
+    grad = ReLU.derivatives(None, None, mu)[1]
+    assert torch.all(grad == 0)
