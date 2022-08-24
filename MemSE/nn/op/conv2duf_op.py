93fc2facdc610c94d64f6069398b5591f97231cb
sebastienwood <sebastien.henwood@polymtl.ca>
2022-08-24 13:50:18 -0400

    bf

--- MemSE/nn/op/conv2duf_op.py
+++ MemSE/nn/op/conv2duf_op.py
@@ -71,8 +71,6 @@ def op_numba(input, gamma, mu, c, weight_shape_1, weight_shape_2, weight_shape_3
                                 i0pii = i0p+ii
                                 oob_0 = i0ii < padding[0] or i0ii >= mu.shape[2] + padding[0]
                                 oob_0p = i0pii < padding[0] or i0pii >= mu.shape[2] + padding[0]
-                                if oob_0 or oob_0p:
-                                    break
                                 for ji in range(weight_shape_3):
                                     j0ji = j0+ji
                                     j0pji = j0p+ji
@@ -80,8 +78,6 @@ def op_numba(input, gamma, mu, c, weight_shape_1, weight_shape_2, weight_shape_3
                                     oob_0p = oob_0p or j0pji < padding[1] or j0pji >= mu.shape[3] + padding[1]
                                     if not oob_0 and not oob_0p:
                                         v += (mu[bi, ci, i0ii, j0ji] * mu[bi, ci, i0pii, j0pji] + gamma[bi, ci, i0ii, j0ji, ci, i0pii, j0pji])
-                                    else:
-                                        break
                         for c0 in range(input.shape[1]):
                             input[bi, c0, i0, j0, c0, i0p, j0p] += c[c0] * v
     return input
