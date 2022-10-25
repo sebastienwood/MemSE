cd0143885c2567abc2bec2b1dd032233a5a8568a
sebastienwood <sebastien.henwood@polymtl.ca>
2022-10-25 14:25:50 -0400

    bf dynamic wmax_mode

--- tests/test_memristorquant.py
+++ tests/test_memristorquant.py
@@ -1,7 +1,7 @@
 from MemSE import METHODS
 from MemSE.definitions import WMAX_MODE
 from MemSE.test_utils import INP as inp, DEVICES, MODELS, SIGMA, get_net_transformed, nn2memse
-
+import torch
 import pytest
 
 @pytest.mark.parametrize("method", METHODS.values())
@@ -16,3 +16,19 @@ def test_columnwise(method, device, mode):
     print(memse.quanter.Wmax)
     memse.quanter.unquant()
     memse.forward(inp.to(device))
+    
+
+@pytest.mark.parametrize("device", DEVICES)
+def test_change(device):
+    net = MODELS['vgg_features']
+    conv2duf, o = get_net_transformed(net, METHODS['unfolded'], inp)
+    conv2duf, o = conv2duf, o.to(device)
+    memse = nn2memse(conv2duf, mode=WMAX_MODE.ALL).to(device)
+    all_wmax = memse.quanter.Wmax
+    memse.quanter.init_wmax('layerwise')
+    layerwise_wmax = memse.quanter.Wmax
+    assert len(torch.unique(layerwise_wmax)) > 1
+    memse.quanter.init_wmax('columnwise')
+    columnwise_wmax = memse.quanter.Wmax
+    for columnwise_wmax_i in columnwise_wmax:
+        assert len(torch.unique(columnwise_wmax_i)) > 1
