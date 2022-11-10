df457ba95b4219e778c205456f14e42721ddd3dd
sebastienwood <sebastien.henwood@polymtl.ca>
2022-11-10 17:00:23 -0500

    distinct file

---
+++ MemSE/misc/model_properties.py
@@ -0,0 +1,14 @@
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters", "Mean"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        param = parameter.numel()
+        table.add_row([name, param, parameter.mean().detach().cpu().item()])
+        total_params += param
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
